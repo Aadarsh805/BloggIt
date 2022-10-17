c19258d00869bb6791938034fc9360433068ece7
aadarsh805 <aadarshthakur80501@gmail.com>
2022-10-17 19:56:56 +0530

    new ui

---
+++ components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import Link from "next/link";
+
+const categories = [
+  { name: "React", slug: "react" },
+  {
+    name: "web dev",
+    slug: "web-dev",
+  },
+];
+
+const Navbar = () => {
+    return (
+        <div className="container mx-auto px-10 mb-8">
+            
+        </div>
+    )
+};
+
+export default Navbar;
---
+++ components/PostUtil.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostUtil = () => {
+  return (
+    <div>PostUtil</div>
+  )
+}
+
+export default PostUtil
