f4683dc4792045ae460e86401b28305fea442aed
aadarsh805 <aadarshthakur80501@gmail.com>
2022-10-18 23:38:11 +0530

    connected to the cms

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <div className="container mx-auto px-10 mb-8">
-            
-        </div>
-    )
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      
+    </div>
+  );
 };
 
 export default Navbar;
